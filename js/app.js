92ec3d9d4a7ec4ea1fd9de21bad5a6cfbf9cf71b
Candelier82 <98266407+Candelier82@users.noreply.github.com>
2025-11-25 10:45:26 +0100

    lektion övning

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+
+const name = 'Lorem Ipsum, Lorem Ipsum,Lorem Ipsum,Lorem ' +
+  '                   Ipsum,Lorem Ipsum,Lorem Ipsum,Lorem Ipsum, ';
+let number = 43;
+number = 39;
+
+document.querySelector('.txt').innerHTML = name;
+
+console.log(`Hej jag heter ${name}
+och jag är ${number} år`);
+console.log("Hello World!");
