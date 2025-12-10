2c7c16825289261bbae8a541cbeec408aabcedd9
callecandelier-maker <callecandelier@gmail.com>
2025-12-10 09:17:01 +0100

    border bottom filter section css

--- js/fetch.js
+++ js/fetch.js
@@ -1,4 +1,4 @@
-
+/*
 const userInput = document.getElementById("userInput");
 const button = document.getElementById("meal-submit");
 
@@ -20,6 +20,60 @@ async function fetchData() {
 
   console.log(res.meals[0]);
 }
-*/
+
 fetchData();
+*/
+
+
+// <input type="text" placeholder="Enter meal here" id="user-input">
+//   <button id="meal-submit">Search</button>
+// <div id="meal-results"></div>
+
+// input
+const userInput = document.getElementById("user-input");
+// knappen
+const btn = document.getElementById("meal-submit");
+
+//Skapa en eventlistener för knappen som lyssnar på click och där vi kan lägga funktionalitet
+btn.addEventListener("click", function(){
+  fetchData(userInput);
+})
+
+async function fetchData(input) {
+  let userIn = input.value;
+  try{
+    const response = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/search.php?s=${userIn}` );
+
+    const json = await response.json();
+    const meals = json.meals;
+    console.log(meals);
+
+    // Hämta huvud-container där vi lägger all data; bilder text
+    const container = document.getElementById("meal-results");
+
+    // Tömmer tidigare inmatningar
+    container.innerHTML = "";
+
+    if(meals){
+      for(let meal of meals){
+        const createDiv = document.createElement("div");
+        createDiv.innerHTML = `
+          <h3>${meal.strMeal}</h3>
+          <img src="${meal.strMealThumb}" alt="${meal.strMeal}" width="200">
+        `;
+        container.appendChild(createDiv);
+
+      }
+
+
+    }
+
+  }
+  catch(error){
+    console.error(error);
+  }
+
+
+}
 
